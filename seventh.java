a948b2a24f854e46564a75de530aea7fe0e8355a
swapnil298 <pandey.swapnil95@gmail.com>
2017-07-09 14:35:42 +0530

    Add files via upload

---
+++ seventh.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class seventh
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i = 0 ; i<3; i++){
+			System.out.println("Hello");
+		}
+	}
+}
