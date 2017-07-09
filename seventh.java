4208bc48421ffcc2f7046d32bcb99a03f0abaa76
swapnil298 <pandey.swapnil95@gmail.com>
2017-07-09 14:36:20 +0530

    Update seventh.java

--- seventh.java
+++ seventh.java
@@ -7,7 +7,7 @@ class seventh
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		for(int i = 0 ; i<3; i++){
+		for(int i = 0 ; i<5; i++){
 			System.out.println("Hello");
 		}
 	}
